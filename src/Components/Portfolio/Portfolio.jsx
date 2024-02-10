7792c9606aed226bc1d92f353bbaa94610e8e61c
Pawan <pawanb78@gmail.com>
2024-02-10 19:00:00 +0000

    new project added

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -5,43 +5,59 @@ import Portfolio2 from '../../assets/portfolio2.png'
 import Portfolio3 from '../../assets/portfolio3.png'
 import Portfolio4 from '../../assets/portfolio4.png'
 import Portfolio5 from '../../assets/portfolio5.png'
+import Portfolio6 from '../../assets/portfolio6.png'
+import Portfolio7 from '../../assets/portfolio7.png'
 
 const Portfolio = () => {
   const portfolio = [
     {
-      id: 5,
+      id: 2,
       Title: "Expense-Tracker",
       image: Portfolio5,
       github: 'https://github.com/bhandaripawan8/Expense-Tracker',
       demo: 'https://expense-tracker-2xp.pages.dev/'
     },
     {
-      id: 4,
+      id: 3,
       Title: "To-Do list",
       image: Portfolio4,
       github: 'https://github.com/bhandaripawan8/ToDo-list',
       demo: 'https://todo-list-cgs.pages.dev/'
     },
     {
-      id: 3,
+      id: 4,
       Title: "Tindog",
       image: Portfolio2,
       github: 'https://github.com/bhandaripawan8/tindog',
       demo: 'https://tindog-ek6.pages.dev/'
     },
     {
-      id: 1,
+      id: 5,
       Title: "NCC-Airline",
       image: Portfolio1,
       github: 'https://github.com/bhandaripawan8/Airlines',
       demo: 'https://airlines.pages.dev/'
     },
     {
-      id: 2,
+      id: 6,
       Title: "Forest",
       image: Portfolio3,
       github: 'https://github.com/bhandaripawan8/forest',
       demo: 'https://forestassignment.pages.dev/'
+    },
+    {
+      id: 7,
+      Title: "Image Gallery",
+      image: Portfolio6,
+      github: 'https://github.com/bhandaripawan8/image-gallery',
+      demo: 'https://github.com/bhandaripawan8/image-gallery'
+    },
+    {
+      id: 1,
+      Title: "Food Recipe",
+      image: Portfolio7,
+      github: 'https://github.com/bhandaripawan8/FoodRecipe',
+      demo: 'https://foodrecipe-3fu.pages.dev/'
     }
   ]
   return (
